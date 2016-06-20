bec279d65fe3a2e66499909851ad1db3604f1d7a
zeropol2 <zeropol2@gmail.com>
2016-06-20 21:17:45 +0900

    set language, timezone

--- ezblog/ezblog/settings.py
+++ ezblog/ezblog/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'  # 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'  # 'UTC'
 
 USE_I18N = True
 
