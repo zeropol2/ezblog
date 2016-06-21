8999a586b5e23eb0f6322ba77730e8015a7d7598
zeropol2 <zeropol2@gmail.com>
2016-06-21 21:04:41 +0900

    add detail page

--- ezblog/blog/urls.py
+++ ezblog/blog/urls.py
@@ -5,5 +5,6 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.posts, name='posts'),
     url(r'^$', views.index, name='index'),
 ]
--- ezblog/blog/views.py
+++ ezblog/blog/views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Post
 
@@ -23,3 +25,31 @@ def index(request):
     }
 
     return render(request, 'list.html', ctx)
+
+
+# posts
+def posts(request, pk):
+    if request.method == 'GET':
+        return __get_post(request, pk)
+    elif request.method == 'POST':
+        raise Http404
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
+    url = reverse('blog:index')
+    return redirect(url)
+
+
+def __get_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    ctx = {
+        'post': post,
+    }
+
+    return render(request, 'detail.html', ctx)
+
+
+
