49f553b71465a45a4ac33e88230148ad50edcbe2
zeropol2 <zeropol2@gmail.com>
2016-06-21 20:47:29 +0900

    customize admin

--- ezblog/blog/admin.py
+++ ezblog/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Post, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', ]
+    list_display_links = ['id', 'title', ]
+    ordering = ['-created_at', 'id', ]
+    search_fields = ['title', 'content', ]
+    date_hierarchy = 'created_at'
+    list_filter = ['tags', ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+
