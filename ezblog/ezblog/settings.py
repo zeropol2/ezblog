bfa6fa68dd8535d8820735313a7eac2ba5c8c769
zeropol2 <zeropol2@gmail.com>
2016-07-23 21:49:52 +0900

    sync static files

--- ezblog/ezblog/settings.py
+++ ezblog/ezblog/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
